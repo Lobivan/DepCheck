af12d6ec7892f0baa16741e5f2a278c20119ee40
Lobivan <79607911231.il@gmail.com>
2024-07-02 17:03:03 +0700

    Add project files.

---
+++ DepCheck/Commands/MyCommand.cs
@@ -0,0 +1,27 @@
+﻿using EnvDTE;
+using System.Diagnostics;
+using System.IO;
+
+namespace DepCheck
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var sol = await VS.Solutions.GetActiveProjectAsync();
+            string solutionDir = Path.GetDirectoryName(sol.FullPath);
+            solutionDir = Path.GetDirectoryName(solutionDir);
+            await VS.MessageBox.ShowWarningAsync(solutionDir);
+
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = $"/k C:\\Users\\79607\\Downloads\\dependency-check\\bin\\dependency-check.bat -o {solutionDir} -s {solutionDir}";
+            proc.StartInfo = startInfo;
+            proc.Start();
+            proc.WaitForExit();
+            System.Diagnostics.Process.Start(solutionDir + "\\dependency-check-report.html");
+        }
+    }
+}
