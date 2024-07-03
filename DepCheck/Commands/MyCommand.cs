809b7eae1d053d043142a778b5e2d5230deafc64
Lobivan <79607911231.il@gmail.com>
2024-07-03 12:00:33 +0700

    Fix moved constructing cmd string into separate method

--- DepCheck/Commands/MyCommand.cs
+++ DepCheck/Commands/MyCommand.cs
@@ -1,6 +1,7 @@
 ﻿using EnvDTE;
 using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace DepCheck
 {
@@ -18,40 +19,50 @@ namespace DepCheck
             string solutionDir = Path.GetDirectoryName(sol.FullPath);
             solutionDir = Path.GetDirectoryName(solutionDir);
 
+            string cmdString = await ConstructCommandStringAsync(solutionDir);
+            if (cmdString == "")
+                return;
+
             System.Diagnostics.Process proc = new System.Diagnostics.Process();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = cmdString;
+            proc.StartInfo = startInfo;
+            proc.Start();
+            proc.WaitForExit();
 
+            System.Diagnostics.Process.Start(solutionDir + "\\dependency-check-report.html");
+        }
+        private async Task<string> ConstructCommandStringAsync(string solutionDir)
+        {
             var opts = await Options.GetLiveInstanceAsync();
+
             char cmdPrefix = 'c';
             if (opts.ShowTrminal == TerminalState.showAndKeepWhenDone)
-            {
                 cmdPrefix = 'k';
-            }
-            else if (opts.ShowTrminal == TerminalState.doNotShow)
-            {
-                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            }
+
             if (File.Exists(opts.DCPath) == false)
             {
-                await VS.MessageBox.ShowWarningAsync($"File {opts.DCPath} does not exist!", 
+                await VS.MessageBox.ShowWarningAsync($"File {opts.DCPath} does not exist!",
                     "Change DC executable in Tools > Options > DepCheck > Paths > Path to DC");
-                return;
+                return "";
             }
+
             if (opts.ReportPath == "")
+            {
                 opts.ReportPath = solutionDir;
+                await opts.SaveAsync();
+            }
+
             if (Directory.Exists(opts.ReportPath) == false)
             {
                 await VS.MessageBox.ShowWarningAsync($"File {opts.ReportPath} does not exist!",
                     "Change Report destination in Tools > Options > DepCheck > Paths > Path to DC Report");
-                return;
+                return "";
             }
 
-            startInfo.Arguments = $"/{cmdPrefix} {opts.DCPath} -o {opts.ReportPath} -s {solutionDir}";
-            proc.StartInfo = startInfo;
-            proc.Start();
-            proc.WaitForExit();
-            System.Diagnostics.Process.Start(solutionDir + "\\dependency-check-report.html");
+            string result = $"/{cmdPrefix} {opts.DCPath} -o {opts.ReportPath} -s {solutionDir}";
+            return result;
         }
     }
 }
